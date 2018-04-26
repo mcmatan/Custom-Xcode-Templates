4c117fffb0c47d3ee0a73134734c253cb7b3f219
matancohen <cohen.abravanel@gmail.com>
2018-04-26 16:46:40 +0300

    Added controller

---
+++ ViewController&ViewModel&Factory.xctemplate/___FILEBASENAME___VC.swift
@@ -0,0 +1,57 @@
+//
+// ___FILENAME___
+// ___PROJECTNAME___
+//
+// Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+/*
+ DISCLAIMER
+ The goal of GitHub's open source licensing efforts is to provide a starting point to help you make an informed choice. GitHub displays license information to help users get information about open source licenses and the projects that use them. We hope it helps, but please keep in mind that we’re not lawyers and that we make mistakes like everyone else. For that reason, GitHub provides the information on an “as-is” basis and makes no warranties regarding any information or licenses provided on or through it, and disclaims liability for damages resulting from using the license information. If you have any questions regarding the right license for your code or any other legal issues relating to it, it’s always best to consult with a professional.
+ */
+
+
+import Foundation
+import UIKit
+
+class ___VARIABLE_productName:identifier___VC: ViewController, IScreen, ___VARIABLE_productName:identifier___ViewModelDelegate {
+    private var viewModel: ___VARIABLE_productName:identifier___ViewModelType
+    
+    init(viewModel: ___VARIABLE_productName:identifier___ViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        self.viewModel.delegate = self
+    }
+    
+    func myScreenIdentifier() -> ScreenIdentifier {
+        return .___VARIABLE_productName:identifier___VC
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setupView() {
+        self.addSubviews()
+        self.layoutViews()
+        self.styleViews()
+    }
+    
+    private func addSubviews() {
+        
+    }
+    
+    private func layoutViews() {
+        
+    }
+    
+    private func styleViews() {
+        
+    }
+    
+    // ViewModel delegate
+    
+    func refresh() {
+        
+    }
+}
---
+++ ViewController&ViewModel&Factory.xctemplate/___FILEBASENAME___ViewFactory.swift
@@ -0,0 +1,27 @@
+//
+// ___FILENAME___
+// ___PROJECTNAME___
+//
+// Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+/*
+ DISCLAIMER
+ The goal of GitHub's open source licensing efforts is to provide a starting point to help you make an informed choice. GitHub displays license information to help users get information about open source licenses and the projects that use them. We hope it helps, but please keep in mind that we’re not lawyers and that we make mistakes like everyone else. For that reason, GitHub provides the information on an “as-is” basis and makes no warranties regarding any information or licenses provided on or through it, and disclaims liability for damages resulting from using the license information. If you have any questions regarding the right license for your code or any other legal issues relating to it, it’s always best to consult with a professional.
+ */
+
+
+import Foundation
+import Swinject
+
+// Don't forget to register 
+class ___VARIABLE_productName:identifier___VCFactory: Factory {
+    func create() -> ___VARIABLE_productName:identifier___VC {
+        let arg: String = "" // Replace this for passed argument
+        self.container.autoregister(___VARIABLE_productName:identifier___ViewModelType.self, argument: String.self, initializer: ___VARIABLE_productName:identifier___ViewModel.init)
+        let viewModel = self.container.resolve(___VARIABLE_productName:identifier___ViewModelType.self, argument: arg)!
+        
+        self.container.autoregister(___VARIABLE_productName:identifier___VC.self, argument: ___VARIABLE_productName:identifier___ViewModelType.self, initializer: ___VARIABLE_productName:identifier___VC.init)
+        return self.container.resolve(___VARIABLE_productName:identifier___VC.self, argument: viewModel)!
+    }
+}
