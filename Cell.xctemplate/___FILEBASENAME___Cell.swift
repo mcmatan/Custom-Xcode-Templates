abc80fdc1f90664694292a1bc8fdab8cc56a37ad
mcmatan <cohen.abravanel@gmail.com>
2018-05-09 15:01:41 +0300

    added cell

---
+++ Cell.xctemplate/___FILEBASENAME___Cell.swift
@@ -0,0 +1,50 @@
+//
+// ___FILENAME___
+// ___PROJECTNAME___
+//
+// Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+/*
+ DISCLAIMER
+ The goal of GitHub's open source licensing efforts is to provide a starting point to help you make an informed choice. GitHub displays license information to help users get information about open source licenses and the projects that use them. We hope it helps, but please keep in mind that we’re not lawyers and that we make mistakes like everyone else. For that reason, GitHub provides the information on an “as-is” basis and makes no warranties regarding any information or licenses provided on or through it, and disclaims liability for damages resulting from using the license information. If you have any questions regarding the right license for your code or any other legal issues relating to it, it’s always best to consult with a professional.
+ */
+
+
+import Foundation
+import UIKit
+
+class ___VARIABLE_productName:identifier___Cell: Cell {
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.addSubviews()
+        self.layoutViews()
+        self.styleViews()
+    }
+    
+    private func addSubviews() {
+        
+    }
+    
+    private func layoutViews() {
+        
+    }
+    
+    private func styleViews() {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    static func getHeight() -> CGFloat {
+        return CGFloat(48)
+    }
+    
+    static func cellIdentifier() -> String {
+        return "___VARIABLE_productName:identifier___Cell"
+    }
+
+}
